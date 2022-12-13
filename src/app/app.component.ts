4e39081fb69df5c401883bf3a2c1bc44b73e28b7
David Nicholas <Dnicholas@bitovi.com>
2022-12-12 17:29:24 -0800

    update project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import 'vite-react-to-webcomponent/webcomponents';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'vite-r2wc-example-angular';
+}
